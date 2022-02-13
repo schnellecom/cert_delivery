57d0ebf9dd649eafa30dc3ee1ac722284fadb11e
schnellecom <schnellecom@ncld4.localdomain>
2022-02-13 14:46:42 +0100

    generalize parameters for general purpose use

--- cert_renew.sh
+++ cert_renew.sh
@@ -2,5 +2,5 @@
 systemctl stop apache2
 certbot renew
 systemctl start apache2
-scp -i /home/schnellecom/.ssh/id_rsa /etc/letsencrypt/live/ncld.home2.eu/* cert-delivery@192.168.1.11:/etc/ssl_keys/
+scp -i [KEY-LOCATION]/id_rsa /etc/letsencrypt/live/[DOMAIN]/* [USER]@[PROXY-SERVER]:[KEY-LOCATION]
 
